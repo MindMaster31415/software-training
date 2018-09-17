a6d3f1ee05c76d1f5b9dfade23cb24fd8adc07f4
MindMaster31415 <sumedhgarimella@gmail.com>
2018-09-17 17:51:20 -0400

    first addition

---
+++ Thanos.java
@@ -0,0 +1,22 @@
+public class Thanos {
+
+	public int snap(int pop) {
+		System.out.println("*snap*");
+		pop /= 2;
+		return pop;
+	}
+
+	public static void main(String[] args) {
+		int population = 0;
+		if (args.length >= 1) {
+			population = Integer.parseInt(args[0]);
+		}
+
+		Thanos thanos = new Thanos();
+
+		System.out.println("The population right now is: " + population);
+		System.out.println("You should've aimed for the head!");
+		population = thanos.snap(population);
+		System.out.println("Now the population is: " + population);
+	}
+}
